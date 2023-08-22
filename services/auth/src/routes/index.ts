1f1a4dbc6e037839868231716ebd78b0fb462ecc
Doublecore <greannakub@gmail.com>
2023-08-22 15:10:56 +0700

    edited package.json

--- services/auth/src/routes/index.ts
+++ services/auth/src/routes/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import authRoute from './authRoute';
+import registerRoute from './registerRoute';
 
 const router: express.Router = express.Router();
 
 export default (): express.Router => {
-  authRoute(router);
+  registerRoute(router);
   return router;
 };
